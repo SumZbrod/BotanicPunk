38fc0a62f779da669687f226aa717e841567a7f6
sumzbrod <sumzbrod@gmail.com>
2026-04-03 17:49:30 +0300

    accelerating jumps

--- script/player.gd
+++ script/player.gd
@@ -1,19 +1,20 @@
 class_name PlayerClass extends CharacterBody2D
 const MAX_SPEED := 800
 const AXCELERATION := 4000
-const JUMP_SPEED := -600
+const JUMP_SPEED := -400
 @export var max_jump_velocity := -200
 @export var jump_acceleration := -5000
-@onready var sprite_animation: AnimationPlayer = $SpriteAnimation
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_area: JumpAreaNode = $JumpArea
 var sprint_mode : bool
 var max_add_jumps_count: int = 1
 var current_add_jumps_count: int = 2
 var sprint_coeff := 1.5
+var jump_max_time := .2
+var jump_time := 0.
 
 func _ready() -> void:
-	sprite_animation.play("IDLE")
+	animated_sprite_2d.play("IDLE")
 
 func _physics_process(delta: float) -> void:
 	_handle_gravity(delta)
@@ -21,7 +22,10 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _handle_gravity(delta):
-	if not is_on_floor():
+	if jump_time > 0:
+		jump_time -= delta
+	elif not is_on_floor():
+		jump_time = 0
 		var gravity = get_gravity() * delta 
 		velocity += gravity
 
@@ -36,13 +40,13 @@ func _handle_input(delta):
 			velocity.x = move_toward(velocity.x, sprint_coeff*direction*MAX_SPEED, AXCELERATION * delta)
 		else:
 			velocity.x = move_toward(velocity.x, direction*MAX_SPEED, AXCELERATION * delta)
-		if sprite_animation.current_animation != "WALK":
-			sprite_animation.play("WALK")
+		if animated_sprite_2d.animation != "WALK":
+			animated_sprite_2d.play("WALK")
 	else:
 		velocity.x = move_toward(velocity.x, 0, AXCELERATION * delta)
 		
-		if sprite_animation.current_animation != "IDLE":
-			sprite_animation.play("IDLE")
+		if animated_sprite_2d.animation != "IDLE":
+			animated_sprite_2d.play("IDLE")
 	if (direction < 0 and animated_sprite_2d.flip_h) or (direction > 0 and !animated_sprite_2d.flip_h):
 		animated_sprite_2d.flip_h = not animated_sprite_2d.flip_h
 		
@@ -51,9 +55,15 @@ func _handle_input(delta):
 			current_add_jumps_count = 0
 			velocity.y = JUMP_SPEED
 			velocity.x /= 2
+			jump_time = jump_max_time
 		elif current_add_jumps_count < max_add_jumps_count:
 			current_add_jumps_count += 1
 			velocity.y = JUMP_SPEED
+			jump_time = jump_max_time
+	elif Input.is_action_just_released("ui_accept"):
+		jump_time = 0
+		if velocity.y < 0:
+			velocity.y /= 2
 
 func is_character_can_jump() -> bool:
 	return jump_area.is_can_jump()
