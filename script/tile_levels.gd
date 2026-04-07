83a5002ecfb8d7acac362555b07f1d6cd0c3a0cf
sumzbrod <sumzbrod@gmail.com>
2026-04-07 21:30:33 +0300

    particles

---
+++ script/tile_levels.gd
@@ -0,0 +1,13 @@
+extends Node2D
+@onready var back_tiles: TileMapLayer = $BackTiles
+@onready var level_tiles: TileMapLayer = $LevelTiles
+
+const chunk_size = 100
+ 
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
